4a6b6492515c431877a646b25554a02a0c823328
Kristinn Sigurðsson <kris.sigur@gmail.com>
2014-08-15 11:23:18 +0000

    Update license header in all source files.

--- deduplicator-commons/src/main/java/is/landsbokasafn/deduplicator/DeDuplicatorConstants.java
+++ deduplicator-commons/src/main/java/is/landsbokasafn/deduplicator/DeDuplicatorConstants.java
@@ -1,3 +1,19 @@
+/* Copyright (C) 2006-2014 National and University Library of Iceland (NULI)
+ * 
+ * This file is part of the DeDuplicator (Heritrix add-on module).
+ * 
+ *  NULI licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package is.landsbokasafn.deduplicator;
 
 public interface DeDuplicatorConstants {
--- deduplicator-heritrix/src/main/java/is/landsbokasafn/deduplicator/heritrix/DeDuplicator.java
+++ deduplicator-heritrix/src/main/java/is/landsbokasafn/deduplicator/heritrix/DeDuplicator.java
@@ -1,24 +1,18 @@
-/* DeDuplicator
- * 
- * Created on 10.04.2006
- *
- * Copyright (C) 2006-2010 National and University Library of Iceland
+/* Copyright (C) 2006-2014 National and University Library of Iceland (NULI)
  * 
  * This file is part of the DeDuplicator (Heritrix add-on module).
  * 
- * DeDuplicator is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * any later version.
- * 
- * DeDuplicator is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser Public License
- * along with DeDuplicator; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  NULI licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package is.landsbokasafn.deduplicator.heritrix;
 
